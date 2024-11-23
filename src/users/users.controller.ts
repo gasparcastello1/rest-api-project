96f3879a8b991ecfbeace2532ae7b75d7140b969
gaspar <v-gcastello@expediagroup.com>
2024-11-23 07:54:48 -0300

    controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get() // /users
+    findAll() {
+        return []
+    }
+
+    @Get(':id') // /users/:id
+    findOne(@Param('id') id: String) {
+        return { id }
+    }
+
+    @Get('interns') // /users/interns
+    findAllInterns() {
+        return []
+    }
+
+    @Post() //Post user
+    createUser(@Body() user: {}) {
+        return user
+    }
+}   
