237fc2f4d7f116cf1d145b12666943fe4d8de694
gaspar <v-gcastello@expediagroup.com>
2024-11-24 14:11:43 -0300

    database module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { ModuleService } from './database/database.service';
+import { EmployeesModule } from './employees/employees.module';
+import { DatabaseModule } from './database/database.module';
+import { ModuleService } from './database/database.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ModuleService],
 })
-export class AppModule {}
+export class AppModule { }
